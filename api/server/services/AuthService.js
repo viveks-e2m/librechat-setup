0f0858afa9cf47b4ad777dba9b2a695eac9db72f
Vivek-Soni-E2M <vivek.soni@e2m.solutions>
2025-07-08 20:58:15 +0530

    Updated - mongo configuration

--- api/server/services/AuthService.js
+++ api/server/services/AuthService.js
@@ -357,6 +357,8 @@ const resetPassword = async (userId, token, password) => {
  * @returns
  */
 const setAuthTokens = async (userId, res, sessionId = null) => {
+  console.log('userId -->', userId, res);
+  
   try {
     const user = await getUserById(userId);
     const token = await generateToken(user);
